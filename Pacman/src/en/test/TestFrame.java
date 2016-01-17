11f78739b4109dacc8e2a573c153f4fd8e16a688
Adrea4m <adrienbourgeois4@gmail.com>
2016-01-17 17:12:04 +0100

    Test_frame

---
+++ Pacman/src/en/test/TestFrame.java
@@ -0,0 +1,9 @@
+package en.test;
+
+import en.window.Frame;
+
+public class TestFrame {
+	public static void main(String[] args) {
+		Frame f = new Frame();
+	}
+}
--- Pacman/src/en/window/Frame.java
+++ Pacman/src/en/window/Frame.java
@@ -1,5 +1,20 @@
 package en.window;
 
-public class Frame {
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
+import javax.swing.JFrame;
+
+public class Frame {
+	private JFrame frame;
+
+	public Frame() {
+		frame = new JFrame();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int height = (int) screenSize.getHeight();
+		frame.setName("Pacman");
+		frame.setSize((4 * height) / 3, height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 }
