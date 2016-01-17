8df8142d856ae7bf1add40b7c4a2c77e82d6cfbd
Adrea4m <adrienbourgeois4@gmail.com>
2016-01-17 18:24:05 +0100

    test_frame 3

--- Pacman/src/en/window/Frame.java
+++ Pacman/src/en/window/Frame.java
@@ -1,24 +1,34 @@
 package en.window;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Frame {
-	private JFrame frame;
+public class Frame extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel background;
 
 	public Frame() {
-		frame = new JFrame();
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int height = (int) (screenSize.getHeight() * 0.90);
-		frame.setName("Pacman");
-		frame.setSize((4 * height) / 3, height);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		this.setTitle("Pacman");
+		this.setSize((4 * height) / 3, height);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		background = new JPanel();
+		background.setBackground(Color.BLACK);
+		this.setContentPane(background);
+		this.setVisible(true);
 	}
-	
+
 	public JFrame getFrame() {
-		return frame;
+		return this;
 	}
 }
