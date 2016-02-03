8602ff4b4cbab42cfe184b861517dfc38b29ed29
Adre4m <adrienbourgeois4@gmail.com>
2016-02-03 16:54:42 +0100

    Mouvement presque entierement codé

--- Pacman/src/en/master/Character.java
+++ Pacman/src/en/master/Character.java
@@ -18,37 +18,64 @@ public class Character {
 		return position;
 	}
 
-	public boolean move(char dir, char[][] lab) {
+	public boolean move(char dir, Game game) {
 		switch (dir) {
 		case 'u':
-			if (lab[position.x - 1][position.y] != 'X' && lab[position.x - 1][position.y] != 'J') {
-				lab[position.x - 1][position.y] = lab[position.x][position.y];
-				lab[position.x--][position.y] = ' ';
+			if (game.getLab()[position.x - 1][position.y] != 'X' && game.getLab()[position.x - 1][position.y] != 'J') {
+				inAnyCase(position.x - 1, position.y, game);
 				return true;
 			}
 			return false;
 		case 'd':
-			if (lab[position.x + 1][position.y] != 'X' && lab[position.x + 1][position.y] != 'J') {
-				lab[position.x + 1][position.y] = lab[position.x][position.y];
-				lab[position.x++][position.y] = ' ';
-				return true;
-			}
-			return false;
-		case 'l':
-			if (lab[position.x][position.y + 1] != 'X' && lab[position.x][position.y + 1] != 'J') {
-				lab[position.x][position.y + 1] = lab[position.x][position.y];
-				lab[position.x][position.y++] = ' ';
+			if (game.getLab()[position.x + 1][position.y] != 'X' && game.getLab()[position.x + 1][position.y] != 'J') {
+				inAnyCase(position.x + 1, position.y, game);
 				return true;
 			}
 			return false;
 		case 'r':
-			if (lab[position.x][position.y - 1] != 'X' && lab[position.x][position.y - 1] != 'J') {
-				lab[position.x][position.y - 1] = lab[position.x][position.y];
-				lab[position.x][position.y--] = ' ';
+			if (game.getLab()[position.x][position.y + 1] != 'X' && game.getLab()[position.x][position.y + 1] != 'J') {
+				inAnyCase(position.x, position.y + 1, game);
+				return true;
+			}
+			return false;
+		case 'l':
+			if (game.getLab()[position.x][position.y - 1] != 'X' && game.getLab()[position.x][position.y - 1] != 'J') {
+				inAnyCase(position.x, position.y - 1, game);
 				return true;
 			}
 			return false;
 		}
 		return true;
+	}
+
+	private boolean instancePacman(int x, int y, Game game) {
+		if (game.getLab()[x][y] == 'G') {
+			if (game.getGhost(x, y).isVunerable()) {
+				return false;
+			} else {
+				((Pacman) this).death();
+				return true;
+			}
+		} else if (game.getLab()[x][y] == 'g')
+			game.setScore(game.getScore() + 10);
+		else if (game.getLab()[x][y] == 'S') {
+			game.setScore(game.getScore() + 50);
+			for (int i = 1; i < game.characters.length; ++i)
+				((Ghost) (game.characters[i])).setVunerable(true);
+		}
+		return false;
+	}
+
+	private void inAnyCase(int x, int y, Game game) {
+		if (this instanceof Pacman)
+			if(instancePacman(x, y, game))
+				return;
+		char old = game.getLab()[x][y];
+		game.getLab()[x][y] = game.getLab()[position.x][position.y];
+		game.getLab()[position.x][position.y] = game.getTopStack(this);
+		position.x = x;
+		position.y = y;
+		if (!(this instanceof Pacman))
+			game.setTopStack(this, old);
 	}
 }
