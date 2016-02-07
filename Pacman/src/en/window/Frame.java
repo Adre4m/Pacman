a4a0bde312baf68e974fcf833e421d1eebda9cf5
vincent <vincent@Vecxinnt>
2016-02-07 13:46:41 +0100

    Ajout actionListener dans Frame pour le bouton start (pour tester)

--- Pacman/src/en/window/Frame.java
+++ Pacman/src/en/window/Frame.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.Box;
 import javax.swing.JButton;
@@ -45,6 +47,20 @@ public class Frame extends JFrame {
 		buttonStyle(start);
 		buttonStyle(score);
 		buttonStyle(opt);
+		start.addActionListener(new ActionListener(){ //Open game
+			public void actionPerformed (ActionEvent e) {
+				  System.out.println("COUCOU");
+				  JFrame gameScreen = new JFrame();
+				  Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+				  int height = (int) (screenSize.getHeight() * 0.90);
+				  gameScreen.setTitle("Pacman");
+				  gameScreen.setSize((4 * height) / 3, height);
+				  gameScreen.setResizable(false);
+				  gameScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				  gameScreen.setLocationRelativeTo(null);
+				  gameScreen.setVisible(true);
+		    	}
+		});
 		menu.add(start);
 		menu.add(score);
 		menu.add(opt);
