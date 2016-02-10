0d9ef044fe5d89ee29c68857089947e791a1bdbf
Gr-Lin <lindsaygrignon@sfr.fr>
2016-02-10 20:01:46 +0100

    Affichage convenable du menu (+logo)

--- Pacman/src/en/window/Frame.java
+++ Pacman/src/en/window/Frame.java
@@ -1,13 +1,21 @@
 package en.window;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.io.File;
+import java.io.IOException;
+
+
+import javax.imageio.ImageIO;
 import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -23,6 +31,8 @@ public class Frame extends JFrame {
 	private JPanel background;
 
 	public Frame() {
+
+		// create frame
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int height = (int) (screenSize.getHeight() * 0.90);
 		this.setTitle("Pacman");
@@ -30,77 +40,78 @@ public class Frame extends JFrame {
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
+
+		// change frame's icon
+		try {
+			this.setIconImage(ImageIO
+					.read(new File("sprites/PacMan_right.gif")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// create frame background
 		background = new JPanel();
 		background.setBackground(Color.BLACK);
-		
-		FlowLayout f = new FlowLayout();
-		f.setAlignment(f.CENTER);
-		background.setLayout(f);
-		background.add(menu());
-		
+		background.setLayout(new BorderLayout());
+		background.add(logo(), BorderLayout.NORTH);
+		background.add(menu(), BorderLayout.CENTER);
+		background.add(copyright(), BorderLayout.SOUTH);
+
 		this.setContentPane(background);
 		this.setVisible(true);
 		while(true);
 	}
-	
-	private Box menu () {
+
+	private JPanel menu() {
+		JPanel button = new JPanel();
+		button.setBackground(null);
+		FlowLayout f = new FlowLayout();
+		f.setAlignment(f.CENTER);
+		button.setLayout(f);
 		Box menu = Box.createVerticalBox();
-		JButton start = new JButton ("Start Game");
-		JButton score = new JButton ("Highscore");
-		JButton opt = new JButton ("Options");
+		JButton start = new JButton("Start Game");
+		JButton score = new JButton("Highscore");
+		JButton opt = new JButton("Options");
 		buttonStyle(start);
 		buttonStyle(score);
 		buttonStyle(opt);
-		start.addActionListener(new ActionListener(){ //Open game
-			public void actionPerformed (ActionEvent e) {
-				  JFrame gameScreen = new JFrame(); //New game screen
-				  Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-				  int height = (int) (screenSize.getHeight() * 0.90);
-				  gameScreen.setTitle("Pacman party");
-				  gameScreen.setSize((4 * height) / 3, height);
-				  gameScreen.setResizable(false);
-				  gameScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				  gameScreen.setLocationRelativeTo(null);
-				  
-				  Game g = new Game(); //load labyrinth
-				  g.init();
-				  JPanel set = new JPanel();
-				  JLabel labyrinth = new JLabel(); //temporary
-				  
-				  set.setBackground(Color.BLACK);
-				  FlowLayout f = new FlowLayout();
-				  f.setAlignment(f.CENTER);
-				  set.setLayout(f);
-				  
-				  String s = "<html>";
-				  for (int i = 0; i < g.getLab().length; ++i) {
-					  for (int j = 0; j < g.getLab()[0].length; ++j)
-						  s += g.getLab()[i][j];
-					  s += "<br>";
-				  }
-				  s+="</html>";
-				  System.out.println(s);
-				  labyrinth.setText(s);
-				  labyrinth.setForeground(Color.BLUE);
-				  
-				  set.add(labyrinth);
-				  gameScreen.add(set);
-				  gameScreen.setContentPane(set);
-				  gameScreen.setVisible(true);
-				  
-		    	}
-		});
 		menu.add(start);
 		menu.add(score);
 		menu.add(opt);
-		return menu;
+		button.add(menu);
+		return button;
 	}
-	
-	private void buttonStyle (JButton b){
+
+	private void buttonStyle(JButton b) {
 		b.setBackground(Color.BLACK);
 		b.setForeground(Color.BLUE);
 		b.setBorder(null);
-		b.setFont(new java.awt.Font("Consolas",1,36));
+		b.setFont(new java.awt.Font("Consolas", 1, 36));
 	}
 
+	private JPanel logo() {
+		JPanel logo = new JPanel();
+		logo.setBackground(null);
+		FlowLayout f = new FlowLayout();
+		f.setAlignment(f.CENTER);
+		logo.setLayout(f);
+		JLabel logoImage = new JLabel();
+		ImageIcon l = new ImageIcon("sprites/Logo.gif");
+		Image img = l.getImage();
+		Image newimg = img.getScaledInstance(700, 350, Image.SCALE_DEFAULT);
+		logoImage.setIcon(new ImageIcon(newimg));
+		logo.add(logoImage);
+		return logo;
+	}
+
+	private JPanel copyright() {
+		JLabel name = new JLabel(
+				"Done by BOURGEOIS Adrien, GRIGNON Lindsay, RIETZ Vincent");
+		name.setForeground(Color.WHITE);
+		JPanel j = new JPanel();
+		j.setBackground(null);
+		j.setLayout(new BorderLayout());
+		j.add(name, BorderLayout.WEST);
+		return j;
+	}
 }
