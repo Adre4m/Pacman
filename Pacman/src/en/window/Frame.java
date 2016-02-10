aa7be9ba589fb80c2f4e2449c8acd4f03d4d7dcc
Gr-Lin <lindsaygrignon@sfr.fr>
2016-02-10 20:11:38 +0100

    Boutton start "fonctionnel"

--- Pacman/src/en/window/Frame.java
+++ Pacman/src/en/window/Frame.java
@@ -75,6 +75,47 @@ public class Frame extends JFrame {
 		buttonStyle(start);
 		buttonStyle(score);
 		buttonStyle(opt);
+		
+		start.addActionListener(new ActionListener(){ //Open game
+			   public void actionPerformed (ActionEvent e) {
+			      JFrame gameScreen = new JFrame(); //New game screen
+			      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			      int height = (int) (screenSize.getHeight() * 0.90);
+			      gameScreen.setTitle("Pacman party");
+			      gameScreen.setSize((4 * height) / 3, height);
+			      gameScreen.setResizable(false);
+			      gameScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			      gameScreen.setLocationRelativeTo(null);
+			      
+			      Game g = new Game(); //load labyrinth
+			      g.init();
+			      JPanel set = new JPanel();
+			      JLabel labyrinth = new JLabel(); //temporary
+			      
+			      set.setBackground(Color.BLACK);
+			      FlowLayout f = new FlowLayout();
+			      f.setAlignment(f.CENTER);
+			      set.setLayout(f);
+			      
+			      String s = "<html>";
+			      for (int i = 0; i < g.getLab().length; ++i) {
+			       for (int j = 0; j < g.getLab()[0].length; ++j)
+			        s += g.getLab()[i][j];
+			       s += "<br>";
+			      }
+			      s+="</html>";
+			      System.out.println(s);
+			      labyrinth.setText(s);
+			      labyrinth.setForeground(Color.BLUE);
+			      
+			      set.add(labyrinth);
+			      gameScreen.add(set);
+			      gameScreen.setContentPane(set);
+			      gameScreen.setVisible(true);
+			      
+			       }
+			  });
+		
 		menu.add(start);
 		menu.add(score);
 		menu.add(opt);
