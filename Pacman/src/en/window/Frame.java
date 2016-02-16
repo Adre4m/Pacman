10ce440dceac9bc78d9804b027408febe5f6c5f5
vincent <vincent@Vecxinnt>
2016-02-16 14:22:46 +0100

    Début d'affichage dans la même fenêtre + alléger un peu le code

--- Pacman/src/en/window/Frame.java
+++ Pacman/src/en/window/Frame.java
@@ -70,46 +70,21 @@ public class Frame extends JFrame {
 		FlowLayout f = new FlowLayout();
 		f.setAlignment(f.CENTER);
 		button.setLayout(f);
-		Box menu = Box.createVerticalBox();
-		JButton start = new JButton("Start Game");
-		JButton score = new JButton("Highscore");
-		JButton opt = new JButton("Options");
+		final Box menu = Box.createVerticalBox();
+		final JButton start = new JButton("Start Game");
+		final JButton score = new JButton("Highscore");
+		final JButton opt = new JButton("Options");
 		buttonStyle(start);
 		buttonStyle(score);
 		buttonStyle(opt);
 		
 		start.addActionListener(new ActionListener(){ //Open game
 			   public void actionPerformed (ActionEvent e) {
-			      JFrame gameScreen = new JFrame(); //New game screen
-			      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-			      int height = (int) (screenSize.getHeight() * 0.90);
-			      gameScreen.setTitle("Pacman party");
-			      gameScreen.setSize((4 * height) / 3, height);
-			      gameScreen.setResizable(false);
-			      gameScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			      gameScreen.setLocationRelativeTo(null);
-			      
-			      Game g = new Game(); //load labyrinth
-			      g.init();
-			      JPanel set = new JPanel();
-			      
-			      set.setBackground(Color.BLACK);
-			      GridLayout f = new GridLayout(g.getLab().length, g.getLab()[0].length);
-			      set.setLayout(f);
-			      
-			      String s="";
-			      for (int i = 0; i < g.getLab().length; ++i) {
-			       for (int j = 0; j < g.getLab()[0].length; ++j){
-			    	   s+=g.getLab()[i][j];
-			    	   set.add(new Button(s));
-			    	   s="";
-			       }
-			      }
-
-			      gameScreen.add(set);
-			      gameScreen.setContentPane(set);
-			      gameScreen.setVisible(true);
-			      
+				  JPanel set = gameScreen();
+			      start.setVisible(false);
+			      score.setVisible(false);
+			      opt.setVisible(false);
+			      menu.add(set);
 			       }
 			  });
 		
@@ -141,7 +116,28 @@ public class Frame extends JFrame {
 		logo.add(logoImage);
 		return logo;
 	}
+	
+	private JPanel gameScreen(){
+		Game g = new Game(); //load labyrinth
+	      g.init();
+	      JPanel set = new JPanel();
+	      
+	      set.setBackground(Color.BLACK);
+	      GridLayout f = new GridLayout(g.getLab().length, g.getLab()[0].length);
+	      set.setLayout(f);
+	      
+	      String s="";
+	      for (int i = 0; i < g.getLab().length; ++i) {
+	       for (int j = 0; j < g.getLab()[0].length; ++j){
+	    	   s+=g.getLab()[i][j];
+	    	   set.add(new Button(s));
+	    	   s="";
+	       }
+	      }
 
+		return set;
+	}
+	
 	private JPanel copyright() {
 		JLabel name = new JLabel(
 				"Done by BOURGEOIS Adrien, GRIGNON Lindsay, RIETZ Vincent");
