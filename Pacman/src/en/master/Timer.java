74c8d8a7b4c78f5dd5955cbb69e557f281a36fa6
Adre4m <adrienbourgeois4@gmail.com>
2016-02-18 18:18:00 +0100

    modification des personnage et du timer

--- Pacman/src/en/master/Timer.java
+++ Pacman/src/en/master/Timer.java
@@ -3,14 +3,15 @@ package en.master;
 public class Timer {
 
 	// 1/FPS pour savoir s'il faut faire l'update
-	public static final double FPS = 60.;
+	public static final long FPS = 60;
 	// GMVPS/FPS pour savoir s'il faut faire bouger le fantome
-	public static final double GMVPS = 2.;
+	public static final long GMVPS = 5;
+	public static final long VMVPS = 2;
 	// PMVPS/FPS pour savoir s'il faut faire bouger Pacman
-	public static final double PMVPS = 1.;
+	public static final long PMVPS = 4;
 	// Le temps en secondes que les fantomes restent en prison au debut du jeu
-	public static final double PRISON = 8.;
+	public static final long PRISON = 8;
 	// La durée d'efficacité d'une supergum
-	public static final double SUPERGUM = 7.;
+	public static final long SUPERGUM = 7;
 
 }
