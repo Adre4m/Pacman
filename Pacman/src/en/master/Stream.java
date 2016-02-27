c40f543cae78b8815c1cffc16cb433839d110475
vincent <vincent@Vecxinnt>
2016-02-27 19:43:13 +0100

    Ajout méthode pour lire les options (lit le fichier par défaut et créé un fichier config au premier lancement de jeu sinon il lit le fichier config)

--- Pacman/src/en/master/Stream.java
+++ Pacman/src/en/master/Stream.java
@@ -1,10 +1,70 @@
 package en.master;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class Stream {
+	
+	public String readConfig(){
+		InputStream is = null;
+		int a;
+		char c;
+		String s = "";
+		boolean firstLaunch = false;
+		
+		try { //To find which file we need to open
+			is = new FileInputStream("options/config.opt");
+		} catch (FileNotFoundException fnfe) {
+		     try {
+		    	 is = new FileInputStream("options/default.opt");
+		    	 firstLaunch = true;
+		     } catch (FileNotFoundException e) {
+		         e.printStackTrace();
+		     }
+		}
+		
+		
+		try { //To read the file
+			// reads till the end of the stream
+			while ((a = is.read()) != -1) {
+				// converts integer to character
+				c = (char) a;
+					s += c;
+			}
+		} catch (Exception e) {
+			// if any I/O error occurs
+			e.printStackTrace();
+		} finally {
+		
+			// releases system resources associated with this stream
+			if (is != null)
+				try {
+					is.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		
+		if(firstLaunch){
+			try{
+				BufferedWriter writer = new BufferedWriter(new FileWriter(new File("options/config.opt")));
+				// if the file doesn't exist, it is created in the root of the project
+				writer.write(s); //copy default in config
+				writer.close();
+			}
+			catch (IOException e)
+			{
+			e.printStackTrace();
+			}
+		}
+		return s;
+	}
 	
 	public String readScore(String path){
 		InputStream is = null;
@@ -16,7 +76,6 @@ public class Stream {
 			// new input stream created
 			is = new FileInputStream(path);
 			
-			boolean b = true;
 			// reads till the end of the stream
 			while ((a = is.read()) != -1) {
 				// converts integer to character
