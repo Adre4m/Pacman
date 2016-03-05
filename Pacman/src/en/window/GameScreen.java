f59d4505258e07d70f9baa6b55746617a9980c5e
vincent <vincent@Vecxinnt>
2016-03-05 15:12:56 +0100

    reglages taille du lab

--- Pacman/src/en/window/GameScreen.java
+++ Pacman/src/en/window/GameScreen.java
@@ -3,8 +3,11 @@ package en.window;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Label;
 import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -18,9 +21,11 @@ import javax.swing.JPanel;
 import en.master.Game;
 
 public class GameScreen extends JLayeredPane{
-	Game g = new Game(); // load labyrinth
+	Game g;
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	int height = (int) (screenSize.getHeight() * 0.95);
+	JPanel grid;
+	JLabel piclabel;
 	
 	public GameScreen(){
 		
@@ -28,13 +33,12 @@ public class GameScreen extends JLayeredPane{
 	    this.setBounds(0, 0, 4 * height /3, height);
 	 
 	    //##########Grid###########
-	    // enlever l'initialisation du jeu des que possible
-		
+	    // enlever l'initialisation du jeu dès que possible
+		g = new Game(); // load labyrinth
 		g.initTest();
-		
-		JPanel grid = new JPanel();
+		grid = new JPanel();
 		grid.setBackground(Color.BLACK);
-	    grid.setBounds(0, 0, 4 * height /3 - 150, height - 50);
+	    grid.setBounds(2*height/10, -15, 4 * height /3 - 4*height/10, height);
 	    grid.setOpaque(true);
 	    grid.setLayout(new GridLayout(32,28));
 	    
@@ -48,17 +52,23 @@ public class GameScreen extends JLayeredPane{
 		}
 		//######################
 	    
-	    
-	    BufferedImage sprite = null;
+	    //Image de fond
+	    BufferedImage before = null;
+	    int w = 0;
+	    int h = 0;
+	    Image after = null; //resized image
 		try {
-			sprite = ImageIO.read(new File("sprites/bh6.png"));
+			before = ImageIO.read(new File("sprites/bh6.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    JLabel piclabel = new JLabel(new ImageIcon(sprite));
-	
-	    piclabel.setBounds(100, 100, 500, 575);
+		after=before.getScaledInstance(4 * height /3 - 4*height/10, height, Image.SCALE_DEFAULT);
+		w = 4 * height /3 - 4*height/10;
+	    h = height;
+
+	    piclabel = new JLabel(new ImageIcon(after));
+	    piclabel.setBounds(2*height/10, 0, w, h);
 	    piclabel.setOpaque(true);
 	    this.add(grid, new Integer(0), 0);
 	    this.add(piclabel, new Integer(1), 0);
