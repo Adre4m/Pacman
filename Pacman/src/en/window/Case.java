a9f509f36a5c70654f94c7509d968c2b09b3c7fc
vincent <vincent@Vecxinnt>
2016-03-08 16:03:38 +0100

    Ajout classe Case pour afficher les sprites + modif GameScreen

---
+++ Pacman/src/en/window/Case.java
@@ -0,0 +1,125 @@
+package en.window;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Case extends JPanel{
+	char content; //type of image
+	JLabel label; //contains the image
+	
+	public Case(char content){
+		URL url; //url of the image
+		Image img; //image without a resize
+		Image newimg; //image resized
+		
+		switch(content){
+		
+			case ' ': 
+				this.label = new JLabel(" ");
+				add(label);
+				break;
+			case 'X': 
+				this.label = new JLabel("X");
+				add(label);
+				break;
+			case 'g':
+				setBackground(Color.BLACK);
+				setLayout(new FlowLayout(FlowLayout.CENTER));
+				label = new JLabel();
+				img = new ImageIcon("sprites/classic/PacGum.gif").getImage();
+				newimg = img.getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+				label.setIcon(new ImageIcon(newimg));
+				add(label);
+				break;
+			case 'S':
+				setBackground(Color.BLACK);
+				setLayout(new FlowLayout(FlowLayout.CENTER));
+				label = new JLabel();
+				img = new ImageIcon("sprites/classic/Super pacgum.gif").getImage();
+				newimg = img.getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+				label = new JLabel(new ImageIcon(newimg));
+				add(label);
+				break;
+			case 'D':
+				this.label = new JLabel("D");
+				break;
+			case 'P':
+				setBackground(Color.BLACK);
+				setLayout(new FlowLayout(FlowLayout.CENTER));
+				label = new JLabel();
+				img = new ImageIcon("sprites/classic/PacMan_right.gif").getImage();
+				newimg = img.getScaledInstance(18, 18, Image.SCALE_DEFAULT);
+//				label.setIcon(new ImageIcon(newimg));
+				label = new JLabel(new ImageIcon(newimg));
+				add(label);
+				break;
+			case 'G':
+				setBackground(Color.BLACK);
+				setLayout(new FlowLayout(FlowLayout.CENTER));
+				label = new JLabel();
+				img = new ImageIcon("sprites/classic/Blinky_down.gif").getImage();
+				newimg = img.getScaledInstance(15, 15, Image.SCALE_DEFAULT);
+				label.setIcon(new ImageIcon(newimg));
+				add(label);
+				break;
+//			case '1': 
+//				setBackground(Color.BLACK);
+//				setLayout(new FlowLayout(FlowLayout.CENTER));
+//				label = new JLabel();
+//				url = getClass().getResource("Super pacgum.gif");
+//				img = new ImageIcon(url).getImage();
+//				newimg = img.getScaledInstance(18, 18, Image.SCALE_DEFAULT);
+////				label.setIcon(new ImageIcon(newimg));
+//				label = new JLabel(new ImageIcon(newimg));
+//				add(label);
+//				System.out.println("lol");
+//				break;
+//			case '2': 
+//				setBackground(Color.BLACK);
+//				setLayout(new FlowLayout(FlowLayout.CENTER));
+//				label = new JLabel();
+//				url = getClass().getResource("Clyde_down.gif");
+//				img = new ImageIcon(url).getImage();
+//				newimg = img.getScaledInstance(30, 20, Image.SCALE_DEFAULT);
+//				label.setIcon(new ImageIcon(newimg));
+//				add(label);
+//				break;
+//			case '3':
+//				setLayout(new FlowLayout(FlowLayout.CENTER));
+//				label = new JLabel();
+//				url = getClass().getResource("Inky_down.gif");
+//				img = new ImageIcon(url).getImage();
+//				newimg = img.getScaledInstance(30, 20, Image.SCALE_DEFAULT);
+//				label.setIcon(new ImageIcon(newimg));
+//				add(label);
+//				break;
+//			case '4':
+//				setBackground(Color.BLACK);
+//				setLayout(new FlowLayout(FlowLayout.CENTER));
+//				label = new JLabel();
+//				url = getClass().getResource("Pinky_down.gif");
+//				img = new ImageIcon(url).getImage();
+//				newimg = img.getScaledInstance(30, 20, Image.SCALE_DEFAULT);
+//				label.setIcon(new ImageIcon(newimg));
+//				add(label);
+//				break;
+			default:
+				break;
+	
+			}
+		
+		this.setBackground(Color.BLACK);
+	}
+	
+}
--- Pacman/src/en/window/GameScreen.java
+++ Pacman/src/en/window/GameScreen.java
@@ -2,6 +2,7 @@ package en.window;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Label;
@@ -24,54 +25,64 @@ public class GameScreen extends JLayeredPane{
 	Game g;
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	int height = (int) (screenSize.getHeight() * 0.95);
-	JPanel grid;
+	JLabel grid;
 	JLabel piclabel;
 	
+	public Case getCase(int x){
+		return (Case) this.grid.getComponent(x);
+		
+	}
 	public GameScreen(){
 		
 		this.setPreferredSize(new Dimension(4 * height /3, height));
 	    this.setBounds(0, 0, 4 * height /3, height);
-	 
-	    //##########Grid###########
-	    // enlever l'initialisation du jeu dès que possible
-		g = new Game(); // load labyrinth
-		g.initTest("labyrinths/labyrinth2.txt");
-		grid = new JPanel();
-		grid.setBackground(Color.BLACK);
-	    grid.setBounds(2*height/10, -15, 4 * height /3 - 4*height/10, height);
-	    grid.setOpaque(true);
-	    grid.setLayout(new GridLayout(32,28));
 	    
-		String s = "";
-		for (int i = 0; i < g.getLab().length; ++i) {
-			for (int j = 0; j < g.getLab()[0].length; ++j) {
-				s += g.getLab()[i][j];
-				grid.add(new Label(s));
-				s = "";
-			}
-		}
-		//######################
-	    
-	    //Image de fond
+	  //Image de fond
 	    BufferedImage before = null;
 	    int w = 0;
 	    int h = 0;
-	    Image after = null; //resized image
+	    ImageIcon after = null; //resized image
 		try {
 			before = ImageIO.read(new File("sprites/bh6.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		after=before.getScaledInstance(4 * height /3 - 4*height/10, height, Image.SCALE_DEFAULT);
+		after=new ImageIcon(before.getScaledInstance(4 * height /3 - 4*height/10, height, Image.SCALE_DEFAULT));
 		w = 4 * height /3 - 4*height/10;
 	    h = height;
-
-	    piclabel = new JLabel(new ImageIcon(after));
+	    
+	    piclabel = new JLabel(after);
 	    piclabel.setBounds(2*height/10, 0, w, h);
 	    piclabel.setOpaque(true);
+	    
+	    //##########Grid###########
+	    // enlever l'initialisation du jeu dès que possible
+		g = new Game(); // load labyrinth
+		g.initTest("labyrinths/labyrinth2.txt");
+		grid = new JLabel(/*after*/);
+		grid.setBackground(Color.BLACK);
+	    grid.setBounds(2*height/10, 0, 4 * height /3 - 4*height/10, height);
+	    grid.setOpaque(true);
+	    grid.setLayout(new GridLayout(32,28));
+//	    grid.setLayout(new GridBagLayout());
+	    Label l;
+		String s = "";
+		for (int i = 0; i < g.getLab().length; ++i) {
+			for (int j = 0; j < g.getLab()[0].length; ++j) {
+				s += g.getLab()[i][j];
+				Case c = new Case(s.charAt(0));
+				grid.add(c);
+				s = "";
+			}
+		}
+		//######################
+	    
+//	    Label l1 = (Label) grid.getComponent(7);
+//	    getCase(7).setText("F");
+	    grid.setVisible(true);
 	    this.add(grid, new Integer(0), 0);
-	    this.add(piclabel, new Integer(1), 0);
+//	    this.add(piclabel, new Integer(0), 0);
 	
 	}
 }
