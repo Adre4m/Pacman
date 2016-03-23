4ce1f69ebede87b7555ed5b2302f763d554cd658
Adre4m <adrienbourgeois4@gmail.com>
2016-03-23 23:37:13 +0100

    Controls souris pas fonctionnels, controles clavier pas fluide

--- Pacman/src/en/master/Main.java
+++ Pacman/src/en/master/Main.java
@@ -1,7 +1,5 @@
 package en.master;
 
-import en.controls.ControlsKey;
-import en.controls.ControlsMouse;
 import en.window.Frame;
 
 public class Main {
@@ -14,20 +12,6 @@ public class Main {
 				Game game = new Game();
 				game.init("labyrinths/labyrinth.txt");
 				f.initGameScreen(game);
-				switch (Stream.readOptions()[1]) {
-				case 0:
-					javax.swing.SwingUtilities.invokeLater(new Runnable() {
-						public void run() {
-							f.addKeyListener(new ControlsKey(game));
-						}
-					});
-					break;
-				case 1:
-					f.addMouseListener(new ControlsMouse(game));
-					break;
-				default:
-					new ControlsKey(game);
-				}
 				game.play(f);
 				f.gameStarted = false;
 				// f.menu();
